98921b54ed90f5ddb6c96661211bbf064513f195
rahuldesai9011 <40596118+rahuldesai9011@users.noreply.github.com>
2018-06-27 12:58:53 +0530

    Update JbkSiteLinkVerify.java

--- src/test/java/com/jbk/test/JbkSiteLinkVerify.java
+++ src/test/java/com/jbk/test/JbkSiteLinkVerify.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement; 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
